23243e8c1c8b711383decc173628b24cb36a6da0
Mikkel Ricky <rimi@aarhus.dk>
2017-12-20 15:10:08 +0100

    POD-6: Added apps stuff

---
+++ apps/block/js/app.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+		var container = document.getElementById('itk-dev-podcast');
+
+		container.innerHTML = '<h1>Podcast</h1>'
+				+ '<p><a href="/itkdev/podcast">Show me the stuff!</a></p>';
+});
---
+++ apps/main/js/app.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+		var container = document.getElementById('itk-dev-podcast');
+
+		container.innerHTML = '<h1>Podcast</h1>';
+});
